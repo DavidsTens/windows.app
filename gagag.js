5010a22dfa16002d64e679e84ee84118177f2d2b
David <bruhpatrick228@gmail.com>
2024-01-06 06:56:37 +0300

    fix position #grey-inner-box to #grey-box @#grey-box can be hidden added #add-page-icon for future function the light mode icon has been changed

--- gagag.js
+++ gagag.js
@@ -1,5 +1,10 @@
-            $(document).ready(function() {
+$(document).ready(function() {
     $('.scroll-back-icon').click(function() {
         $('.grey-box').toggle();
+        if (!$('.grey-box').is(':visible')) {
+            $('.grey-inner-box').css('margin-bottom', '10px');
+        } else {
+            $('.grey-inner-box').css('margin-bottom', '0');
+        }
     });
 });
